045c629d5a4a0e682a301eb40b67fadda4122d73
mnikolasd <59995013+mnikolasd@users.noreply.github.com>
2020-06-11 13:49:07 +0200

    Add files via upload

---
+++ WEB Application for a local library/ProjekatBibliotekaPPP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjekatBibliotekaPPP.MvcApplication" Language="C#" %>
---
+++ WEB Application for a local library/ProjekatBibliotekaPPP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjekatBibliotekaPPP.Startup))]
+namespace ProjekatBibliotekaPPP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
